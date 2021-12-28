bf8da6f645d9ed3131da58faeb22f3b29032556d
yeongminseo <kspia0522@naver.com>
2021-12-28 21:12:16 +0900

    project start

---
+++ main.js
@@ -0,0 +1,18 @@
+let str= `
+
+010-36250-4630
+tdkfsdfsd@naver.com the 
+The rest day
+오모리기치찌개 개마시상
+aabbccssa.aThe
+fuck you fsef fus
+fus
+박예은바보_똥개멍청이
+`
+
+//const regexp = new RegExp("the","gi")
+const regexp = /rest|day/  //g = 전체검색 i=대소문자 구별 안함
+
+
+console.log(str.replace(/\s/g,""));
+console.log(str)
